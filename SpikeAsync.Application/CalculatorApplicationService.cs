96b83b2b6c89e590744cb6ee6ef5fbb6e6790449
hsuzu <hsuzu@VOSTRO>
2021-12-12 12:55:25 +0900

    プロジェクト ファイルを追加します。

---
+++ SpikeAsync.Application/CalculatorApplicationService.cs
@@ -0,0 +1,27 @@
+﻿using SpikeAsync.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SpikeAsync.Application
+{
+    public class CalculatorApplicationService
+    {
+        public Calculator NewCalculator()
+        {
+            return new Calculator();
+        }
+
+        public void Run(Calculator calculator)
+        {
+            calculator.Run();
+        }
+
+        public void Cancel(Calculator calculator)
+        {
+            calculator.Cancel();
+        }
+    }
+}
---
+++ SpikeAsync.WPF/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using Prism.Mvvm;
+using Reactive.Bindings;
+using Reactive.Bindings.Extensions;
+using SpikeAsync.Application;
+using System;
+
+namespace SpikeAsync.WPF.ViewModels
+{
+    public class MainWindowViewModel : BindableBase
+    {
+        private string _title = "Prism Application";
+        private readonly CalculatorApplicationService _calculatorApplicationService;
+
+        public ReactiveCommand RunCommand { get; }
+        public ReactiveCommand CancelCommand { get; }
+
+        public ReactiveProperty<int> Result { get; }
+
+        public string Title
+        {
+            get { return _title; }
+            set { SetProperty(ref _title, value); }
+        }
+
+        public MainWindowViewModel()
+        {
+            _calculatorApplicationService = new CalculatorApplicationService();
+
+            var calculator = _calculatorApplicationService.NewCalculator();
+            RunCommand = new ReactiveCommand().WithSubscribe(() => _calculatorApplicationService.Run(calculator));
+            CancelCommand = new ReactiveCommand().WithSubscribe(() => _calculatorApplicationService.Cancel(calculator));
+
+            Result = calculator.ToReactivePropertyAsSynchronized(x => x.Result);
+        }
+    }
+}
