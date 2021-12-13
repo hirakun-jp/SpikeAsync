048506f4e8c17a3bdb12401e5dd0c4107c63e6c7
suzuki hiraku <h.suzuki.immu@gmail.com>
2021-12-13 13:53:07 +0900

    commit

--- SpikeAsync.Application/CalculatorApplicationService.cs
+++ SpikeAsync.Application/CalculatorApplicationService.cs
@@ -1,9 +1,4 @@
 ﻿using SpikeAsync.Domain;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SpikeAsync.Application
 {
--- SpikeAsync.WPF/ViewModels/CalculatorListViewModel.cs
+++ SpikeAsync.WPF/ViewModels/CalculatorListViewModel.cs
@@ -1,10 +1,6 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
+﻿using Prism.Mvvm;
 using Reactive.Bindings;
 using SpikeAsync.Application;
-using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace SpikeAsync.WPF.ViewModels
 {
